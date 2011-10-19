39dd08e1b84c9aecf4eb296db51f25f4b2ff925c
Jeremy Huddleston <jeremyhu@apple.com>
2011-10-19 01:09:30 -0700

    Build fix for ABI Version 12
    ABI Version 12 removes support for multiple PCI domains.  If you need to
    use this driver on a system with more than one PCI domain, you should
    either port this driver to using libpciaccess directly or stick with an
    older server.
    
    Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

--- src/tseng_mode.c
+++ src/tseng_mode.c
@@ -41,7 +41,11 @@ vgaHWWriteBank(vgaHWPtr hwp, CARD8 value)
     if (hwp->MMIOBase)
 	MMIO_OUT8(hwp->MMIOBase, hwp->MMIOOffset + VGA_BANK, value);
     else
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
 	outb(hwp->PIOOffset + VGA_BANK, value);
+#else
+	pci_io_write8(hwp->io, VGA_BANK, value);
+#endif
 }
 
 CARD8
@@ -50,7 +54,11 @@ vgaHWReadBank(vgaHWPtr hwp)
     if (hwp->MMIOBase)
 	return MMIO_IN8(hwp->MMIOBase, hwp->MMIOOffset + VGA_BANK);
     else
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
 	return inb(hwp->PIOOffset + VGA_BANK);
+#else
+	return pci_io_read8(hwp->io, VGA_BANK);
+#endif
 }
 
 #define VGA_SEGMENT 0x3CD
@@ -61,7 +69,11 @@ vgaHWWriteSegment(vgaHWPtr hwp, CARD8 value)
     if (hwp->MMIOBase)
 	MMIO_OUT8(hwp->MMIOBase, hwp->MMIOOffset + VGA_SEGMENT, value);
     else
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
 	outb(hwp->PIOOffset + VGA_SEGMENT, value);
+#else
+	pci_io_write8(hwp->io, VGA_SEGMENT, value);
+#endif
 }
 
 CARD8
@@ -70,7 +82,11 @@ vgaHWReadSegment(vgaHWPtr hwp)
     if (hwp->MMIOBase)
 	return MMIO_IN8(hwp->MMIOBase, hwp->MMIOOffset + VGA_SEGMENT);
     else
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
 	return inb(hwp->PIOOffset + VGA_SEGMENT);
+#else
+	return pci_io_read8(hwp->io, VGA_SEGMENT);
+#endif
 }
 
 /*
@@ -84,8 +100,12 @@ vgaHWWriteModeControl(vgaHWPtr hwp, CARD8 value)
     if (hwp->MMIOBase)
         MMIO_OUT8(hwp->MMIOBase,
                   hwp->MMIOOffset + hwp->IOBase + VGA_MODE_CONTROL, value);
-    else  
-        outb(hwp->IOBase + hwp->PIOOffset + VGA_MODE_CONTROL, value);
+    else
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
+	outb(hwp->PIOOffset + VGA_MODE_CONTROL, value);
+#else
+	pci_io_write8(hwp->io, VGA_MODE_CONTROL, value);
+#endif
 }
 
 /*
@@ -110,14 +130,22 @@ vgaHWHerculesSecondPage(vgaHWPtr hwp, Bool Enable)
 
         MMIO_OUT8(hwp->MMIOBase, hwp->MMIOOffset + VGA_HERCULES, tmp);
     } else {
-        tmp = inb(hwp->PIOOffset + VGA_HERCULES);
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
+	tmp = inb(hwp->PIOOffset + VGA_HERCULES);
+#else
+	tmp = pci_io_read8(hwp->io, VGA_HERCULES);
+#endif
 
         if (Enable)
             tmp |= 0x02;
         else
             tmp &= ~0x02;
 
-        outb(hwp->PIOOffset + VGA_HERCULES, tmp);
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
+	outb(hwp->PIOOffset + VGA_HERCULES, tmp);
+#else
+	pci_io_write8(hwp->io, VGA_HERCULES, tmp);
+#endif
     }
 }
 
